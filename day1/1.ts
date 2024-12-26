0b8d22f5b623da56060f6d8e9b975792a7c07ebe
manuel moreno <manuarchena@gmail.com>
2024-12-26 02:30:49 +0100

    refactor: 1.ts into a more testable solution

--- day1/1.ts
+++ day1/1.ts
@@ -1,17 +1,11 @@
-const problemData: [number[], number[]] = readProblemData ('./input');
+import { Deno } from '@deno/shim-deno';
 
-const sortedProblemData = problemData.map((arrayOfNumbers) => {
-    return arrayOfNumbers.sort((a,b) => a-b)
-});
+export function calculateResultFromFile(filePath: string): number {
+  const problemData = readProblemData(filePath);
+  return calcResult(problemData);
+}
 
-let result: number = 0;
-sortedProblemData[0].forEach((number: number, index: number) => {
-    result += Math.abs(number - sortedProblemData[1][index]);
-})
-
-console.log(result);
-
-function readProblemData (filePath: string): [number[], number[]] {
+function readProblemData(filePath: string): [number[], number[]] {
   const problemData: [number[], number[]] = [[], []];
 
   const inputData = Deno.readTextFileSync(filePath);
@@ -20,10 +14,30 @@ function readProblemData (filePath: string): [number[], number[]] {
   for (const line of lines) {
     const [first, second] = line.trim().split(/\s+/).map(Number);
 
+    if (first === undefined || second === undefined) {
+      throw new Error('Invalid input data');
+    }
+  
     problemData[0].push(first);
     problemData[1].push(second);
   }
   return problemData;
 }
 
+function calcResult(problemData: [number[], number[]]): number {
+  const sortedProblemData = problemData.map((arrayOfNumbers) => {
+    return arrayOfNumbers.sort((a,b) => a-b);
+  });
+
+  let result = 0;
+  sortedProblemData[0].forEach((number: number, index: number) => {
+    result += Math.abs(number - sortedProblemData[1][index]);
+  });
+
+  return result;
+}
+
+if (import.meta.main) {
+  console.log(calculateResultFromFile('./input'));
+}
 
